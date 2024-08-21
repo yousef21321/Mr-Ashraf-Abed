6d2baae3d6737ba4a89526359440b9f79dfdd60c
yousef21321 <popoyouseg@gmail.com>
2024-08-21 14:56:19 +0300

    last

--- src/APPS.js
+++ src/APPS.js
@@ -526,7 +526,7 @@ function APPS() {
     width: "100%",
     height: "170vh",
     position: "absolute",
-    top: "34%",
+    top: "30%",
     background: "linear-gradient(180deg, rgba(96, 128, 191, 0) 0%, rgba(96, 128, 191, 0.25) 25%, rgba(96, 128, 191, 0.50) 50%, #6080BF 100%)",
     backdropFilter: "blur(13px)"
 }}></div>
